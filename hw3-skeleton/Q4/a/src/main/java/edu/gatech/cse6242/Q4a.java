7df8dc12d5d5c5a3504c82ade056e445ad199f34
Ahmed Eissa <ahmed.t.eissa@gmail.com>
2020-03-04 12:07:38 -0500

    h3q4 done

--- hw3-skeleton/Q4/a/src/main/java/edu/gatech/cse6242/Q4a.java
+++ hw3-skeleton/Q4/a/src/main/java/edu/gatech/cse6242/Q4a.java
@@ -11,18 +11,76 @@ import java.io.IOException;
 
 public class Q4a {
 
+	public static class firstMapper extends Mapper<Object, Text, Text, Text>{
+		private Text out = new Text("1");
+		private Text in = new Text("-1");
+	    	private Text pickup = new Text();
+		private Text dropoff = new Text();
+
+	    	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+		      	String[] parts = value.toString().split("\\t");
+
+			pickup.set(parts[0]);
+			dropoff.set(parts[1]);
+			context.write(pickup, out);
+			context.write(dropoff, in);
+		}
+	  }
+
+	public static class myReducer extends Reducer<Text,Text,Text, Text> {
+	    	private Text result = new Text();
+
+	    	public void reduce(Text key, Iterable<Text> values, Context context ) throws IOException, InterruptedException {
+			int sum = 0;
+			for (Text val: values){
+				sum += Integer.parseInt(val.toString());
+			}
+			result.set(Integer.toString(sum));
+			context.write(key, result);
+		}
+	}
+
+	public static class secondMapper extends Mapper<Object, Text, Text, Text>{
+		private Text count = new Text("1");
+		private Text newKey = new Text();
+
+	    	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			String[] parts = value.toString().split("\\t");
+		     	newKey.set(parts[1]);
+			context.write(newKey, count);
+		}
+	  }		
+
   public static void main(String[] args) throws Exception {
 
-    /* TODO: Update variable below with your gtid */
-    final String gtid = "gburdell3";
+	final String gtid = "aeissa3";
 
-    Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "Q4a");
+	Configuration conf = new Configuration();
 
-    /* TODO: Needs to be implemented */
+	Job job1 = Job.getInstance(conf, "first");
 
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
+	job1.setJarByClass(Q4a.class);
+	job1.setMapperClass(firstMapper.class);
+	job1.setReducerClass(myReducer.class);
+	job1.setOutputKeyClass(Text.class);
+    	job1.setOutputValueClass(Text.class);
+
+	FileInputFormat.addInputPath(job1, new Path(args[0]));
+	FileOutputFormat.setOutputPath(job1, new Path("./data/temp"));
+	//System.exit(job1.waitForCompletion(true) ? 0 : 1);
+
+	job1.waitForCompletion(true);
+
+	Job job2 = Job.getInstance(conf, "second");
+
+	job2.setJarByClass(Q4a.class);
+	job2.setMapperClass(secondMapper.class);
+	job2.setReducerClass(myReducer.class);
+	job2.setOutputKeyClass(Text.class);
+    	job2.setOutputValueClass(Text.class);
+
+	FileInputFormat.addInputPath(job2, new Path("./data/temp/part-r-00000"));
+	FileOutputFormat.setOutputPath(job2, new Path(args[1]));
+	System.exit(job2.waitForCompletion(true) ? 0 : 1);
   }
 }
--- hw3-skeleton/Q4/b/src/main/java/edu/gatech/cse6242/Q4b.java
+++ hw3-skeleton/Q4/b/src/main/java/edu/gatech/cse6242/Q4b.java
@@ -11,14 +11,52 @@ import java.io.IOException;
 
 public class Q4b {
 
+  public static class myMapper extends Mapper<Object, Text, Text, Text>{
+		
+    	private Text k = new Text();
+	private Text v = new Text();
+
+    	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+	      	String[] parts = value.toString().split("\\t");
+		k.set(parts[2]);
+		v.set(parts[3]);
+		context.write(k, v);
+	}
+  }
+
+  public static class myReducer
+	extends Reducer<Text,Text,Text, Text> {
+	    	private Text result = new Text();
+
+	    	public void reduce(Text key, Iterable<Text> values, Context context ) 
+		throws IOException, InterruptedException {
+	      	double sum = 0;
+		String temp;
+		double counter = 0.0;
+		for (Text val : values) {
+			sum += Double.parseDouble(val.toString());
+			counter++;
+		}
+		double d = Math.round(sum/counter*100.0)/100.0;
+		result.set(Double.toString(d));
+		context.write(key, result);
+	}
+  }
+
+
   public static void main(String[] args) throws Exception {
-    /* TODO: Update variable below with your gtid */
-    final String gtid = "gburdell3";
+   
+    final String gtid = "aeissa3";
 
     Configuration conf = new Configuration();
     Job job = Job.getInstance(conf, "Q4b");
 
-    /* TODO: Needs to be implemented */
+	job.setJarByClass(Q4b.class);
+	job.setMapperClass(myMapper.class);
+	job.setReducerClass(myReducer.class);
+	job.setOutputKeyClass(Text.class);
+    	job.setOutputValueClass(Text.class);
+
 
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
